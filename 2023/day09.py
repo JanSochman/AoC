d7e9b61b60950b795668db57ce769cad022542a4
Jan Sochman <jan.sochman@fel.cvut.cz>
2023-12-09 09:40:37 +0100

    day 9

--- 2023/day09.py
+++ 2023/day09.py
@@ -26,41 +26,42 @@ def parse_input(input):
 # %% --------------------------------------------------
 # PART 1
 year = 2023
-puzzle_day = 8
+puzzle_day = 9
 puzzle = Puzzle(year=year, day=puzzle_day)
 print(puzzle.input_data[:20])
 
-example1 = """LLR
-
-AAA = (BBB, BBB)
-BBB = (AAA, ZZZ)
-ZZZ = (ZZZ, ZZZ)"""
+example1 = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
 
 data_example = parse_input(example1)
 data_full = parse_input(puzzle.input_data)
 
 # %% --------------------------------------------------
 def part1(data):
-    emap = {}
-    instructions = data[0].strip()
-    position = 'AAA'
-    for L in data[2:]:
-        start, left, right = re.findall(r"(\w+)", L)
-        emap[start] = [left, right]
-    there = False
-    steps = 0
-    while not there:
-        for i in instructions:
-            if i == 'L':
-                position = emap[position][0]
-            else:
-                position = emap[position][1]
-            steps += 1
-            if position == 'ZZZ':
-                there = True
-                break
-            
-    return steps
+    total = 0
+    for L in data:
+        hists = []
+        hist = np.array(L.split(), np.int64)
+        while hist.any():
+            hists.append(hist[-1])
+            hist = hist[1:] - hist[:-1]
+        total += sum(hists)
+    return total
+    
+# def part1(data):
+#     sum = 0
+#     for L in data:
+#         hists = []
+#         hist = np.array(L.split(), np.int64)
+#         while hist.any():
+#             hists.append(hist.copy())
+#             hist = hist[1:] - hist[:-1]
+#         next_val = 0
+#         for h in reversed(hists):
+#             next_val += h[-1]
+#         sum += next_val
+#     return sum
 
 # %% --------------------------------------------------
 res_example = part1(data_example)
@@ -72,47 +73,21 @@ print(res_full)
 
 # %% --------------------------------------------------
 # PART 2
-example2 = """LR
-
-11A = (11B, XXX)
-11B = (XXX, 11Z)
-11Z = (11B, XXX)
-22A = (22B, XXX)
-22B = (22C, 22C)
-22C = (22Z, 22Z)
-22Z = (22B, 22B)
-XXX = (XXX, XXX)"""
+example2 = example1
 
 data_example = parse_input(example2)
 
 # %% --------------------------------------------------
 def part2(data):
-    emap = {}
-    instructions = data[0].strip()
-    positions = [L[:3] for L in data[2:] if L[2] == 'A']
-    print(positions)
-    N = len(positions)
-    for L in data[2:]:
-        start, left, right = re.findall(r"(\w+)", L)
-        emap[start] = [left, right]
-    cycle_lengths = []
-    for j in range(N):
-        there = False
-        steps = 0
-        while not there:
-            for i in instructions:
-                if i == 'L':
-                    positions[j] = emap[positions[j]][0]
-                else:
-                    positions[j] = emap[positions[j]][1]
-                steps += 1
-                if positions[j][2] == 'Z':
-                    there = True
-                    cycle_lengths.append(steps)
-                    break
-    steps = np.lcm.reduce(cycle_lengths)
-            
-    return steps
+    total = 0
+    for L in data:
+        hists = []
+        hist = np.array(L.split(), np.int64)
+        while hist.any():
+            hists.append(hist[0])
+            hist = hist[:-1] - hist[1:]
+        total += sum(hists)
+    return total
 
 # %% --------------------------------------------------
 res_example = part2(data_example)
