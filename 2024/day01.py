219096d00dc1d5a0ed8d569db11ad70b56d68e6d
Jan Sochman <jan.sochman@fel.cvut.cz>
2024-12-01 14:14:42 +0100

    day 1 solution

---
+++ 2024/day01.py
@@ -0,0 +1,100 @@
+# %% ----------------------------
+import numpy as np
+from aocd.models import Puzzle      # easy loading the puzzle data
+from rich import print
+# import parse                        # easy string parsing 
+from parse import parse
+from types import SimpleNamespace as sn
+# from utils import StateMachine      # a skeleton for building a state machine and run it
+from itertools import combinations  # returns all k-combinations of a list elements
+import re                           # for parsing using regular expressions
+from anytree import Node, RenderTree    # for building trees
+from scipy import ndimage
+from scipy.spatial.distance import cdist
+import copy
+from collections import deque, Counter, defaultdict, namedtuple
+import parsy as ps
+import portion as P
+import math
+import matplotlib.pyplot as plt
+from skimage.morphology import flood_fill
+import cv2
+from shapely.geometry import Polygon
+from einops import rearrange
+import networkx as nx
+import heapq
+
+# for regular expressions debugging: https://pythex.org/
+
+# %% --------------------------------------------------
+def parse_input(input):
+    lines = input.split('\n')
+    return lines
+
+# %% --------------------------------------------------
+# ========== PART 1 ==========
+year = 2024
+puzzle_day = 1
+puzzle = Puzzle(year=year, day=puzzle_day)
+print(puzzle.input_data[:200])
+
+example1 = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+
+data_example = parse_input(example1)
+data_full = parse_input(puzzle.input_data)
+
+# %% --------------------------------------------------
+def part1(data):
+    l_list = []
+    r_list = []
+    for L in data:
+        left, right = L.split('   ')
+        heapq.heappush(l_list, int(left))
+        heapq.heappush(r_list, int(right))
+
+    res = 0
+    for _ in range(len(l_list)):
+        res += abs(heapq.heappop(l_list) - heapq.heappop(r_list))
+    return res
+
+# %% --------------------------------------------------
+res_example = part1(data_example[:])
+print(res_example)
+
+# %% --------------------------------------------------
+res_full = part1(data_full[:])
+print(res_full)
+
+# %% --------------------------------------------------
+# ========== PART 2 ==========
+example2 = example1
+
+data_example = parse_input(example2)
+
+# %% --------------------------------------------------
+def part2(data):
+    l_list = []
+    r_counter = Counter()
+    for L in data:
+        left, right = L.split('   ')
+        l_list.append(left)
+        r_counter[right] += 1
+
+    res = sum([int(id) * r_counter[id] for id in l_list])
+
+    return res
+
+# %% --------------------------------------------------
+res_example = part2(data_example[:])
+print(res_example)
+
+# %% --------------------------------------------------
+res_full = part2(data_full[:])
+print(res_full)
+
+# %% --------------------------------------------------
